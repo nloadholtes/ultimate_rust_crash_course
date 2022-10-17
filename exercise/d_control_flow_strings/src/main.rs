ec65296b57a3a8a4d6e1a3fcce08294a9cb041e5
Nick Loadholtes <nick.loadholtes@datarobot.com>
2022-10-17 14:07:44 -0400

    Tracking my work as I go through the course

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -17,6 +17,11 @@ fn main() {
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
+        match arg.as_str() {
+            "sum" => sum(),
+            "double" => double(),
+            _ => count(arg),
+        }
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
@@ -31,7 +36,9 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
+    for x in 7..=23 {
+        sum += x;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,6 +49,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
+    while x < 500 {
+        x *= 2;
+        count+=1;
+    }
 
 
     println!("You can double x {} times until x is larger than 500", count);
@@ -51,7 +62,12 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    let mut counter = 0;
+    loop {
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        counter += 1;
+        if counter == 8{ break}
+    }
 
 
     println!(); // This will output just a newline at the end for cleanliness.
